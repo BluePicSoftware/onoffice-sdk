c0ae6b3072ae1c210ab837f73614054bf7a56db7
Lukas Laptop <forti@DESKTOP-J4SIUQA>
2022-05-09 16:31:25 +0200

    v6

--- src/client/ProxyOnOfficeAPIClient.ts
+++ src/client/ProxyOnOfficeAPIClient.ts
@@ -30,7 +30,7 @@ export default class ProxyOnOfficeAPIClient extends BaseOnOfficeAPIClient {
           } as IAction,
           {
             headers: {
-              "Authorization": this.jwt
+              "Authorization": "JWT " + this.jwt
             }
           }
         );
