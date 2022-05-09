532aaee1400d7c55ef3b93982258fdf419f4c6fc
Lukas Laptop <forti@DESKTOP-J4SIUQA>
2022-05-09 14:51:21 +0200

    debugging

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -38,13 +38,13 @@ export async function fetchActions(
       actions: actions.map((action) => signAction(action, token, secret)),
     },
   };
-
+  console.log(payload);
   const response = await axios.post(
     endpoint,
     payload,
     { responseType }
   );
-
+  console.log(response);
   if (response.status != 200) {
     throw "API call failed with status: " + response.status;
   }
