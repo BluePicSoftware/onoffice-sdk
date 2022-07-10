3b71ac4c4845a0bdd737fdf1466a566ec2fef7c9
Ohm <49402476+kiloOhm@users.noreply.github.com>
2022-07-10 12:59:20 +0200

    wip

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,23 +1,5 @@
 import CryptoJS from 'crypto-js';
 
-export function detectEnv() {
-  if (typeof window !== "undefined" && typeof window.document !== "undefined") {
-    return "browser";
-  } else {
-    return "node";
-  }
-}
-
-// still needed?
-export const webcrypto = new Promise(async (resolve) => {
-  if (detectEnv() === "node") {
-    // @ts-expect-error no dynamic imports
-    import("crypto").then((cryptoModule) => resolve(cryptoModule.webcrypto));
-  } else {
-    resolve(window.crypto);
-  }
-}) as Promise<Crypto>;
-
 export function hmac(value: string, secret: string) {
   //this is the only way I've found to emulate php hash_hmac in binary mode
   return CryptoJS.HmacSHA256(value, secret).toString(CryptoJS.enc.Base64);
