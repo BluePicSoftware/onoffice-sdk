219f4114d97538167773e8c72dc404ef2af5dc3f
Ohm <49402476+kiloOhm@users.noreply.github.com>
2022-07-18 00:31:48 +0200

    pls

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,7 +1,6 @@
-import HmacSHA256 from 'crypto-js/hmac-sha512';
-import Base64 from 'crypto-js/enc-base64';
+import crypto from 'crypto-js';
 
 export function hmac(value: string, secret: string) {
   //this is the only way I've found to emulate php hash_hmac in binary mode
-  return HmacSHA256(value, secret).toString(Base64);
+  return crypto.HmacSHA256(value, secret).toString(crypto.enc.Base64);
 }
